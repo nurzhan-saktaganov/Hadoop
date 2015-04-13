1e6caed02a32b701ae0a1c83a959d32c01b7f7e5
Nurzhan Saktaganov <nur92world@mail.ru>
2015-04-13 14:57:16 +0300

    final

--- hw/hw3/hive/HiveSQL.sql
+++ hw/hw3/hive/HiveSQL.sql
@@ -1,8 +1,24 @@
-CREATE EXTERNAL TABLE cite (citing BIGINT, cited BIGINT) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' STORED AS TEXTFILE;
-	
-LOAD DATA LOCAL INPATH '/user/n.saktaganov/patents/cite75_99.txt' OVERWRITE INTO TABLE cite;
+CREATE EXTERNAL TABLE cite (citing BIGINT, cited BIGINT)
+	ROW FORMAT DELIMITED
+	FIELDS TERMINATED BY ','
+	STORED AS TEXTFILE;
 
-LOAD DATA INPATH '/user/n.saktaganov/patents/cite75_99.txt' OVERWRITE INTO TABLE cite;
+LOAD DATA INPATH '/user/n.saktaganov/patents/cite75_99.txt'
+	OVERWRITE INTO TABLE cite;
 
-LOAD DATA INPATH '/user/n.saktaganov/patents/cite75_99.txt' INTO TABLE cite;
+CREATE EXTERNAL TABLE apat(patent BIGINT, gyear INT
+	, gdate INT, appyear STRING, country STRING)
+	ROW FORMAT DELIMITED
+	FIELDS TERMINATED BY ','
+	STORED AS TEXTFILE;
+
+LOAD DATA INPATH '/user/n.saktaganov/patents/apat63_99.txt'
+	OVERWRITE INTO TABLE apat;
+
+SELECT cited, COUNT(1) AS rate
+	FROM apat JOIN cite ON (apat.patent = cite.citing)
+	WHERE gyear=1990 AND country='"US"'
+	GROUP BY cited
+	ORDER BY rate DESC
+	LIMIT 10;
 
